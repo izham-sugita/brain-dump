e169028d51dc9a9b763ca6e3e6645ad6fd3528af
izham-sugita <30282614+izham-sugita@users.noreply.github.com>
2017-07-19 15:08:47 +0900

    OpenBLAS function dgemm in FORTRAN90
    How to compile.
    Depending on your machine setting. For dynamic linking (linking to libopenblas.so)
    
    gfortran -o filename filename.f90 -lopenblas -lpthread
    
    For static linking (bigger binary file, so be careful)
    gfortran -o filename filename.f90 /your/path/to/libopenblas.a -lpthread
    
    The compilation was tested on Ubuntu 16.04 LTS. OpenBLAS library was installed using apt.

---
+++ blas-test-1.f90
@@ -0,0 +1,79 @@
+program blas1
+  implicit none
+  real(8), allocatable, dimension(:) :: a, b, c
+  real(8), allocatable, dimension(:,:) :: matA, matB, matC
+  real(8) :: alpha, beta
+  real(8) :: error
+  
+  integer :: i, j, imax, jmax
+  integer, dimension(8) :: values
+  integer :: seed
+  integer :: index
+
+  character(len=1) :: transa, transb
+
+  imax = 1024
+  jmax = 1024
+
+  call date_and_time(VALUES=values) !values(8) = milisecs of the second
+  seed = values(8)
+  call srand(seed) !not a std implementation, but i like it better.
+  !print*, values
+  print*, rand(), rand(), rand()
+  
+  allocate(a(imax*jmax))
+  allocate(b(imax*jmax))
+  allocate(c(imax*jmax))
+  allocate(matA(imax,jmax))
+  allocate(matB(imax,jmax))
+  allocate(matC(imax,jmax))
+
+  !initiating array
+  print*, 'Initiating arrays....'
+  do j = 1, jmax
+     do i = 1, imax
+        index = (j-1)*imax + i
+        a(index) = rand()
+        b(index) = rand()
+        c(index) = 0.0
+     end do
+  end do
+
+  do j = 1, jmax
+     do i = 1, imax
+        index = (j-1)*imax + i
+        matA(i,j) = a(index)
+        matB(i,j) = b(index)
+        matC(i,j) = 0.0
+     end do
+  end do
+  
+  alpha = 1.0
+  beta = 0.0
+  transa = 'N'
+  transb = 'N'
+  call dgemm(transa,transb,imax,jmax,imax,alpha,a,imax,b,imax,beta,c,imax)
+
+  print*, 'Checking for correctness....'
+  matC = matmul(matA, matB)
+
+  i = 10
+  j = 10
+  index = (j-1)*imax + i
+  print*, 'matC(',i,',',j,') = ', matC(i,j)
+  print*, 'c(',index,') = ', c(index)
+
+  error = 0.0
+  do j = 1, jmax
+     do i = 1, imax
+        index = (j-1)*imax + i
+        error = error + abs(matC(i,j) - c(index))
+     end do
+  end do
+  print*, error
+  
+  print*, 'Deallocating arrays....'
+  deallocate(a,b,c)
+  deallocate(matA,matB,matC)
+  
+end program blas1
