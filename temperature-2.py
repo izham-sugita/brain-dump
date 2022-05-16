26146c9db54a64629af790635e35a52f41b070ad
Muhammad Izham <30282614+izham-sugita@users.noreply.github.com>
2022-05-17 01:18:04 +0800

    New sample for temperature check

---
+++ temperature-2.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+from matplotlib import style
+import numpy as np
+import time
+import os
+
+#print(plt.style.available)
+#style.use('fivethirtyeight')
+style.use('seaborn')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+fig.canvas.set_window_title("Temperature monitor")
+
+def animate(i):
+    graph_data = open('./data/example.txt', 'r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append( float(x) )
+            ys.append( float(y) )
+
+    ax1.clear()
+    ax1.plot(xs, ys)
+
+
+x = [0.0, 0.0]
+y =[0.0,0.0]
+temp_min = 35.0
+temp_max = 70.0
+dt = 1.0
+iterat2sec = int(20.0/dt)
+plt.axis( [0.0, 20.0, temp_min, temp_max] )
+
+os_type = os.popen("uname -a").read()
+lookforstring = "raspberrypi"
+if lookforstring in os_type:
+    string = "vcgencmd measure_temp"
+    istart = 5
+    iend = 8
+else:
+    print("Not a Pi")
+    string = "sensors | grep \"Core 0\" "
+    istart = 16
+    iend = 20
+        
+    
+def animate2(i):
+    x[0] = x[1]
+    y[0] = y[1]
+    stream = os.popen(string)
+    output = stream.read()
+    temp = float(output[istart:iend])
+    y[1] = temp
+    x[1] = float(i+1)
+
+    if x[1] > 20.0:
+        plt.axis( [ (i-iterat2sec+1)*dt, (i + 1)*dt, temp_min, temp_max ]  )
+
+    ax1.plot(x,y,"bo-")
+
+
+
+#interval = 1000 is for 1000 milisecs
+#ani = anim.FuncAnimation(fig, animate, interval=1000)
+ani = anim.FuncAnimation(fig, animate2, interval=1000)
+plt.show()
